63b33efb971a836a618a32171320d126f1c52b1c
Julien <juliencaron@protonmail.com>
2020-07-22 04:22:57 +0200

    Parallelize createPage fn to reduce build time

--- gatsby-node.js
+++ gatsby-node.js
@@ -24,54 +24,56 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   // Range allMdx into "post" or "page"
   const { posts, pages } = separateAllMdx(results.data.allMdx.edges)
 
-  /**
-   * Create posts
-   */
-  for (let i = 0; i < posts.length; i++) {
-    const post = posts[i]
-    const gist = await getGist(post)
-    const next = posts[i + 1] ? posts[i + 1] : posts[0]
-    const prev = posts[i - 1] ? posts[i - 1] : posts[posts.length - 1]
-
-    createPage({
-      path: `${post.frontmatter.path}/`,
-      component: path.resolve(`./src/templates/post.tsx`),
-      context: {
-        gist,
-        postId: post.id,
-        nextId: next.id,
-        prevId: prev.id,
-      },
-    })
-  }
-
-  /**
-   * Create posts list
-   */
+  // For postList pagination
   const postsPerPage = 5
   const numPages = Math.ceil(posts.length / postsPerPage)
 
-  Array.from({ length: numPages }).forEach((_, i) => {
-    createPage({
-      path: i === 0 ? `/` : `/${i + 1}`,
-      component: path.resolve(`./src/templates/postList.tsx`),
-      context: {
-        numPages,
-        currentPage: i + 1,
-        skip: i * postsPerPage,
-        limit: postsPerPage,
-      },
-    })
-  })
+  await Promise.all([
+    /**
+     * Create posts
+     */
+    ...posts.map(async (post, i) => {
+      const gist = await getGist(post)
+      const next = posts[i + 1] ? posts[i + 1] : posts[0]
+      const prev = posts[i - 1] ? posts[i - 1] : posts[posts.length - 1]
 
-  /**
-   * Create pages
-   */
-  pages.forEach(({ id, frontmatter }) => {
-    createPage({
-      path: `${frontmatter.path}/`,
-      component: path.resolve(`./src/templates/page.tsx`),
-      context: { pageId: id },
-    })
-  })
+      createPage({
+        path: `${post.frontmatter.path}/`,
+        component: path.resolve(`./src/templates/post.tsx`),
+        context: {
+          gist,
+          postId: post.id,
+          nextId: next.id,
+          prevId: prev.id,
+        },
+      })
+    }),
+
+    /**
+     * Create posts list
+     */
+    ...Array.from({ length: numPages }).map((_, i) => {
+      createPage({
+        path: i === 0 ? `/` : `/${i + 1}`,
+        component: path.resolve(`./src/templates/postList.tsx`),
+        context: {
+          numPages,
+          currentPage: i + 1,
+          skip: i * postsPerPage,
+          limit: postsPerPage,
+        },
+      })
+    }),
+
+    /**
+     * Create pages
+     */
+    ...pages.map(({ id, frontmatter }) => {
+      createPage({
+        path: `${frontmatter.path}/`,
+        component: path.resolve(`./src/templates/page.tsx`),
+        context: { pageId: id },
+      })
+    }),
+  ])
 }
