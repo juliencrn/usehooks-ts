4ae4747c3387f29cbe354dca2bc849ed7c8395f1
Julien <juliencaron@protonmail.com>
2020-07-24 04:24:19 +0200

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -120,10 +120,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#007ACC`,
+        theme_color: `#007ACC`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/typescript.png`, // This path is relative to the root of the site.
       },
     },
     {
