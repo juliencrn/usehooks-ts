7e9b27122d69f82b357c7cf7844982c2924d4e00
Julien <juliencaron@protonmail.com>
2020-11-14 16:14:42 +0100

    Add useDebounce Hooks in the repo

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+
+export default function useDebounce<T>(value: T, delay?: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value)
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [value, delay || 500])
+
+  return debouncedValue
+}
