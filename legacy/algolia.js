776147e61fc683e3e9c9285018ce22737e4f60bb
Julien <juliencaron@protonmail.com>
2020-11-15 03:20:36 +0100

    Fix legacy algolia

--- legacy/algolia.js
+++ legacy/algolia.js
@@ -1,32 +1,63 @@
-const postQuery = `{
-  posts: allMdx(
-    limit: 1000,
-    filter: { fields: { type: { eq:"post" } } }
-  )
-    nodes {
-      id
-      excerpt(pruneLength: 155)
-      shortDescription: excerpt(pruneLength: 280)
-      fields {
-        path
+const postQuery = `
+  {
+    algoliaPosts: allMdx(limit: 1000, filter: {fields: {type: {eq: "post"}}}) {
+      nodes {
+        id
+        excerpt(pruneLength: 155)
+        fields {
+          path
+          hookName
+        }
+        frontmatter {
+          title
+        }
       }
-      frontmatter {
-        title
+    }
+    hooks: allMdx(limit: 1000, filter: {fields: {type: {eq: "hook"}}}) {
+      nodes {
+        id
+        fields {
+          hookName
+        }
+      }
+    }
+    demos: allMdx(limit: 1000, filter: {fields: {type: {eq: "demo"}}}) {
+      nodes {
+        id
+        fields {
+          hookName
+        }
       }
     }
   }
-}`
+`
 
-function transformer({ data }) {
-  return data.posts.nodes
-    .filter(node => !node.frontmatter.gistId || !node.frontmatter.gistFilename)
-    .map(({ frontmatter, shortDescription, excerpt, fields }) => ({
-      id: frontmatter.path,
-      path: `/react-hook${fields.path}`,
-      title: frontmatter.title,
-      shortDescription,
-      excerpt,
-    }))
+const transformer = ({ data }) => {
+  const matchesPosts = []
+  data.algoliaPosts.nodes.forEach(post => {
+    const { fields } = post
+
+    // Check if have the corresponding hook
+    const hook = data.hooks.nodes.find(
+      ({ fields: { hookName } }) => hookName === fields.hookName,
+    )
+
+    // Check if have the corresponding hook demo
+    const demo = data.demos.nodes.find(
+      ({ fields: { hookName } }) => hookName === fields.hookName,
+    )
+
+    if (hook && demo) {
+      matchesPosts.push(post)
+    }
+  })
+
+  return matchesPosts.map(({ frontmatter, excerpt, fields }) => ({
+    id: fields.path,
+    path: `/react-hook${fields.path}`,
+    title: frontmatter.title,
+    excerpt,
+  }))
 }
 
 const algoliaQueries = [
