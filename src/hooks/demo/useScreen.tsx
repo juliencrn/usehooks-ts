cea6aa5c52c37426b5305aa7b6cde6c843205495
Julien <juliencaron@protonmail.com>
2020-11-15 01:11:42 +0100

    Add useScreen

---
+++ src/hooks/demo/useScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import useScreen from '../useScreen'
+
+export default function Component() {
+  const screen = useScreen()
+
+  return (
+    <div>
+      The current window dimensions are:{' '}
+      <code>
+        {JSON.stringify({ width: screen?.width, height: screen?.height })}
+      </code>
+    </div>
+  )
+}
