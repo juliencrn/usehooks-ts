e9c72d7d8c215f1fc94c716403b4e47bbc8b7ffb
Julien <juliencaron@protonmail.com>
2021-03-14 19:50:56 +0100

    Add CopyButton for code

---
+++ src/components/CopyButton.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react'
+import { Theme, withStyles } from '@material-ui/core/styles'
+
+import Button from '@material-ui/core/Button'
+import Tooltip from '@material-ui/core/Tooltip'
+import FileCopyIcon from '@material-ui/icons/FileCopy'
+
+const ThemedTooltip = withStyles((theme: Theme) => ({
+  tooltip: {
+    color: theme.palette.common.white,
+    backgroundColor: theme.dracula.comment,
+    boxShadow: theme.shadows[1],
+  },
+}))(Tooltip)
+
+const copy = async (text: string): Promise<true | undefined> => {
+  if (typeof navigator === 'undefined') return
+  if (!navigator.clipboard) return
+
+  try {
+    await navigator.clipboard.writeText(text)
+    return true
+  } catch (error) {
+    console.error('copy failed', error)
+    return
+  }
+}
+
+interface PropTypes {
+  value: string
+  classNames?: string
+}
+
+const CopyButton = ({ value, classNames }: PropTypes) => {
+  const [open, setOpen] = useState(false)
+
+  // Auto close tooltip
+  useEffect(() => {
+    if (open) {
+      const id = setTimeout(() => setOpen(false), 2000)
+      return () => clearTimeout(id)
+    }
+  }, [open])
+
+  const handleCopy = async () => {
+    const copied = await copy(value)
+    if (copied) setOpen(true)
+  }
+
+  return (
+    <ThemedTooltip
+      PopperProps={{
+        disablePortal: true,
+      }}
+      open={open}
+      disableFocusListener
+      disableHoverListener
+      disableTouchListener
+      title="Copied!"
+    >
+      <Button
+        className={classNames}
+        onClick={handleCopy}
+        size="small"
+        startIcon={<FileCopyIcon />}
+      >
+        Copy
+      </Button>
+    </ThemedTooltip>
+  )
+}
+
+export default CopyButton
--- src/components/code.tsx
+++ src/components/code.tsx
@@ -7,13 +7,22 @@ import Typography from '@material-ui/core/Typography'
 // TODO: Test that: https://github.com/dracula/highlightjs/tree/b80c704d6b081ec259d8e5b2ddf2925bd06e71bf
 // TODO: Test that: https://draculatheme.com/prism/
 import theme from '~/libs/prismjs-theme-dracula'
+import CopyButton from './CopyButton'
 // import theme from 'prism-react-renderer/themes/vsDark'
 
 const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    position: 'relative',
+  },
+  copyButton: {
+    position: 'absolute',
+    top: theme.spacing(2),
+    right: theme.spacing(2),
+  },
   pre: {
     textAlign: 'left',
-    margin: theme.spacing(3, 0),
-    padding: theme.spacing(2, 1),
+    margin: theme.spacing(2, 0),
+    padding: theme.spacing(3, 2),
     borderRadius: 4,
     fontFamily: 'Fira Code, monospace',
     overflowX: 'auto',
@@ -23,7 +32,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     display: 'inline-block',
     width: theme.spacing(4),
     userSelect: 'none',
-    opacity: '0.3',
+    color: theme.dracula.comment,
   },
 }))
 
@@ -34,45 +43,49 @@ export interface CodeProps {
 
 const Code = ({ code, language = 'tsx' }: CodeProps) => {
   const classes = useStyles()
-  return (
-    <Highlight
-      {...defaultProps}
-      code={code.trim()}
-      theme={theme}
-      language={language}
-    >
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <Typography
-          variant="body2"
-          component="pre"
-          className={className + ' ' + classes.pre}
-          style={style}
-        >
-          {tokens.map((line, i) => {
-            // Remove the last empty line:
-            let LineNumberElem
-            if (
-              line.length === 1 &&
-              line[0].empty === true &&
-              i === tokens.length - 1
-            ) {
-              LineNumberElem = null
-            } else {
-              LineNumberElem = <span className={classes.line}>{i + 1}</span>
-            }
 
-            return (
-              <div key={i} {...getLineProps({ line, key: i })}>
-                {LineNumberElem}
-                {line.map((token, key) => (
-                  <span key={key} {...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            )
-          })}
-        </Typography>
-      )}
-    </Highlight>
+  return (
+    <div className={classes.root}>
+      <CopyButton classNames={classes.copyButton} value={code} />
+      <Highlight
+        {...defaultProps}
+        code={code.trim()}
+        theme={theme}
+        language={language}
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <Typography
+            variant="body2"
+            component="pre"
+            className={className + ' ' + classes.pre}
+            style={style}
+          >
+            {tokens.map((line, i) => {
+              // Remove the last empty line:
+              let LineNumberElem
+              if (
+                line.length === 1 &&
+                line[0].empty === true &&
+                i === tokens.length - 1
+              ) {
+                LineNumberElem = null
+              } else {
+                LineNumberElem = <span className={classes.line}>{i + 1}</span>
+              }
+
+              return (
+                <div key={i} {...getLineProps({ line, key: i })}>
+                  {LineNumberElem}
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              )
+            })}
+          </Typography>
+        )}
+      </Highlight>
+    </div>
   )
 }
 
