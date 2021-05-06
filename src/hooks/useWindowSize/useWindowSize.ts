849fa718ccab65b6406de9df34c17e59f671457b
Julien <juliencaron@protonmail.com>
2021-05-06 16:53:37 +0200

    🐛 Fix useWindowSize SSR issue

--- src/hooks/useWindowSize/useWindowSize.ts
+++ src/hooks/useWindowSize/useWindowSize.ts
@@ -1,26 +1,35 @@
-// See: https://usehooks-typescript.com/react-hook/use-event-listener
-
-import { useState } from 'react'
-import { useEventListener } from '~/hooks'
+import { useEffect, useState } from 'react'
 
 interface WindowSize {
   width: number
   height: number
 }
 
-const isClient = typeof window !== 'undefined'
-
 function useWindowSize(): WindowSize {
-  const getWindowSize = () => ({
-    width: isClient ? window.innerWidth : 0,
-    height: isClient ? window.innerHeight : 0,
+  const [windowSize, setWindowSize] = useState<WindowSize>({
+    width: 0,
+    height: 0,
   })
 
-  const [windowSize, setWindowSize] = useState<WindowSize>(getWindowSize())
+  useEffect(() => {
+    const handler = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      })
+    }
 
-  useEventListener('resize', () => {
-    setWindowSize(getWindowSize())
-  })
+    // Set size at the first client-side load
+    handler()
+
+    window.addEventListener('resize', handler)
+
+    // Remove event listener on cleanup
+    return () => {
+      window.removeEventListener('resize', handler)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return windowSize
 }
