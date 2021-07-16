4e17d776ecf6ddd10d76b06a1543b052d07d773c
Julien <juliencaron@protonmail.com>
2021-07-16 02:00:21 +0200

    🔥  Remove unused code in wrapPageElement

--- src/libs/wrapPageElement.tsx
+++ src/libs/wrapPageElement.tsx
@@ -1,18 +1,9 @@
 import React, { ReactNode } from 'react'
 
-import { CssBaseline } from '@material-ui/core'
-import { ThemeProvider } from '@material-ui/styles'
-
 import Layout from '../components/layout'
-import themes from '../theme'
 
 const wrapPageElement = ({ element }: { element: ReactNode }) => {
-  return (
-    <ThemeProvider theme={themes['light']}>
-      <CssBaseline />
-      <Layout>{element}</Layout>
-    </ThemeProvider>
-  )
+  return <Layout>{element}</Layout>
 }
 
 export default wrapPageElement
