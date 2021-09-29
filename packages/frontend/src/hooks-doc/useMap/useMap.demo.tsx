8afca9a1f4feaeda3b6d616b66a0ce9f76b5efc8
Julien <juliencaron@protonmail.com>
2021-09-29 23:59:45 +0200

    Add demo for useMap

--- packages/frontend/src/hooks-doc/useMap/useMap.demo.tsx
+++ packages/frontend/src/hooks-doc/useMap/useMap.demo.tsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
-import { useMap } from 'usehooks-ts'
+import { MapOrEntries, useMap } from 'usehooks-ts'
+
+const initialValues: MapOrEntries<string, string> = [['key', '🆕']]
+const otherValues: MapOrEntries<string, string> = [
+  ['hello', '👋'],
+  ['data', '📦'],
+]
 
 export default function Component() {
-  const [map /*, methods */] = useMap<string, string>([['key', 'value']])
+  const [map, actions] = useMap<string, string>(initialValues)
+
+  const set = () => actions.set(String(Date.now()), '📦')
+  const setAll = () => actions.setAll(otherValues)
+  const reset = () => actions.reset()
+  const remove = () => actions.remove('hello')
 
   return (
     <div>
-      <code>{JSON.stringify(map.values)}</code>
+      <button onClick={set}>Add</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={setAll}>Set new data</button>
+      <button onClick={remove} disabled={!map.get('hello')}>
+        {'Remove "hello"'}
+      </button>
+
+      <pre>
+        Map (
+        {Array.from(map.entries()).map(([key, value]) => (
+          <Fragment key={key}>{`\n  ${key}: ${value}`}</Fragment>
+        ))}
+        <br />)
+      </pre>
     </div>
   )
 }
