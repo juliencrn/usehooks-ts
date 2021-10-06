a77bd190e13a17678bf5f20d7f93bf280abfe295
Julien <juliencaron@protonmail.com>
2021-10-06 15:13:45 +0200

    Add tests for useLocalStorage

--- loadershim.js
+++ loadershim.js
@@ -1,3 +1,13 @@
 global.___loader = {
   enqueue: jest.fn(),
 }
+
+global.console = {
+  warn: jest.fn(),
+
+  // Keep native behaviour for other methods, use those to print out things in your own tests, not `console.log`
+  error: console.error,
+  info: console.info,
+  debug: console.debug,
+  log: console.log,
+}
