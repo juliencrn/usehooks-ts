15fdd17b2fdc7c09b87cc7a9c8b2bc43f42e20fc
Julien <juliencaron@protonmail.com>
2021-10-19 04:42:32 +0200

    Update useInternal & useTimeout #40

--- packages/usehooks-ts/src/useTimeout/useTimeout.ts
+++ packages/usehooks-ts/src/useTimeout/useTimeout.ts
@@ -1,17 +1,17 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useLayoutEffect, useRef } from 'react'
 
 function useTimeout(callback: () => void, delay: number | null) {
   const savedCallback = useRef(callback)
 
   // Remember the latest callback if it changes.
-  useEffect(() => {
+  useLayoutEffect(() => {
     savedCallback.current = callback
   }, [callback])
 
   // Set up the timeout.
   useEffect(() => {
     // Don't schedule if no delay is specified.
-    if (delay === null) {
+    if (!delay) {
       return
     }
 
