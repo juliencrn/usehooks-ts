20e2e4b0faecaf5cd05876bbae0a01adc69a387b
Julien <juliencaron@protonmail.com>
2021-11-06 19:27:03 +0100

    fix(useStep): Perform code review

--- lib/src/useStep/useStep.test.ts
+++ lib/src/useStep/useStep.test.ts
@@ -6,47 +6,47 @@ describe('useStep()', () => {
   test('should use step', () => {
     const { result } = renderHook(() => useStep(2))
 
-    expect(result.current.actualStep).toBe(1)
-    expect(typeof result.current.goToNextStep).toBe('function')
-    expect(typeof result.current.goToPrevStep).toBe('function')
-    expect(typeof result.current.setStep).toBe('function')
-    expect(typeof result.current.reset).toBe('function')
-    expect(typeof result.current.canGoToNextStep).toBe('boolean')
-    expect(typeof result.current.canGoToPrevStep).toBe('boolean')
+    expect(result.current[0]).toBe(1)
+    expect(typeof result.current[1].goToNextStep).toBe('function')
+    expect(typeof result.current[1].goToPrevStep).toBe('function')
+    expect(typeof result.current[1].setStep).toBe('function')
+    expect(typeof result.current[1].reset).toBe('function')
+    expect(typeof result.current[1].canGoToNextStep).toBe('boolean')
+    expect(typeof result.current[1].canGoToPrevStep).toBe('boolean')
   })
 
   test('should increment step', () => {
     const { result } = renderHook(() => useStep(2))
 
     act(() => {
-      result.current.goToNextStep()
+      result.current[1].goToNextStep()
     })
 
-    expect(result.current.actualStep).toBe(2)
+    expect(result.current[0]).toBe(2)
   })
 
   test('should decrement step', () => {
     const { result } = renderHook(() => useStep(2))
 
     act(() => {
-      result.current.setStep(2)
+      result.current[1].setStep(2)
     })
 
     act(() => {
-      result.current.goToPrevStep()
+      result.current[1].goToPrevStep()
     })
 
-    expect(result.current.actualStep).toBe(1)
+    expect(result.current[0]).toBe(1)
   })
 
   test('should reset step', () => {
     const { result } = renderHook(() => useStep(2))
 
     act(() => {
-      result.current.reset()
+      result.current[1].reset()
     })
 
-    expect(result.current.actualStep).toBe(1)
+    expect(result.current[0]).toBe(1)
   })
 
   test('should set step', () => {
@@ -55,25 +55,25 @@ describe('useStep()', () => {
     const newStep = 2
 
     act(() => {
-      result.current.setStep(newStep)
+      result.current[1].setStep(newStep)
     })
 
-    expect(result.current.actualStep).toBe(newStep)
+    expect(result.current[0]).toBe(newStep)
   })
 
   test('should return if prev step is available', () => {
     const { result } = renderHook(() => useStep(2))
 
     act(() => {
-      result.current.setStep(2)
+      result.current[1].setStep(2)
     })
 
-    expect(result.current.canGoToPrevStep).toBe(true)
+    expect(result.current[1].canGoToPrevStep).toBe(true)
   })
 
   test('should return if next step is available', () => {
     const { result } = renderHook(() => useStep(2))
 
-    expect(result.current.canGoToNextStep).toBe(true)
+    expect(result.current[1].canGoToNextStep).toBe(true)
   })
 })
