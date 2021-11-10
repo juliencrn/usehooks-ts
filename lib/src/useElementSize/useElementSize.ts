7f54dda1d000b7582a9a51bc64ec833d186e67db
Mark Terrel <mark@unbounded.systems>
2021-11-10 11:08:57 -0700

    fix(useElementSize): useElementSize doesn't update size when elementRef.current changes

--- lib/src/useElementSize/useElementSize.ts
+++ lib/src/useElementSize/useElementSize.ts
@@ -15,25 +15,25 @@ function useElementSize<T extends HTMLElement = HTMLDivElement>(
     width: 0,
     height: 0,
   })
+  const node = elementRef?.current
 
   // Prevent too many rendering using useCallback
   const handleSize = useCallback(() => {
-    const node = elementRef?.current
     if (node) {
       setSize({
         width: node.offsetWidth || 0,
         height: node.offsetHeight || 0,
       })
     }
-  }, [elementRef])
+  }, [node])
 
   useEventListener('resize', handleSize)
 
-  // Initial size on mount
+  // Initial size on mount and when current element changes
   useLayoutEffect(() => {
     handleSize()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, [node])
 
   return size
 }
