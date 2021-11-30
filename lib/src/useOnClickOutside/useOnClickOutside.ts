a376bbdbd42507032212bcdd4c2d43edf63107fe
James Barrett <mail@jamesbarrett.io>
2021-11-30 17:23:11 +0000

    add param to allow 'mouseup' event

--- lib/src/useOnClickOutside/useOnClickOutside.ts
+++ lib/src/useOnClickOutside/useOnClickOutside.ts
@@ -7,8 +7,9 @@ type Handler = (event: MouseEvent) => void
 function useOnClickOutside<T extends HTMLElement = HTMLElement>(
   ref: RefObject<T>,
   handler: Handler,
+  mouseEvent: 'mousedown' | 'mouseup' = 'mousedown'
 ): void {
-  useEventListener('mousedown', event => {
+  useEventListener(mouseEvent, event => {
     const el = ref?.current
 
     // Do nothing if clicking ref's element or descendent elements
