8c7f3bc2368faa6981ed27bce2ff07d282c095c3
didde <didrik.lind@purplescout.se>
2022-01-01 03:43:42 +0100

    test(useDebounce): prettify the tests a bit

--- lib/src/useDebounce/useDebounce.test.ts
+++ lib/src/useDebounce/useDebounce.test.ts
@@ -11,30 +11,32 @@ describe('useDebounce()', () => {
     const value = 'value'
     const {
       result: { current: debounceValue },
-    } = renderHook(() => useDebounce(value, 500))
+    } = renderHook(() => useDebounce(value))
 
     expect(value).toBe(debounceValue)
   })
 
-  test('should debounce with default 500 ms', () => {
+  test('should debounce with default debounce 500 ms', () => {
     mockSetTimeout()
     renderHook(() => useDebounce('value'))
+
     expect(setTimeout).toHaveBeenCalledTimes(1)
     expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 500)
   })
 
-  test('should debounce with given debounce 1337 ms', () => {
+  test('should debounce with given debounce', () => {
     mockSetTimeout()
     renderHook(() => useDebounce('value', 1337))
+
     expect(setTimeout).toHaveBeenCalledTimes(1)
     expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1337)
   })
 
   test('should call clearTimeout on unmount', () => {
-    jest.useFakeTimers()
-    jest.spyOn(global, 'clearTimeout')
+    mockClearTimeout()
     const { unmount } = renderHook(() => useDebounce('value'))
     unmount()
+
     expect(clearTimeout).toHaveBeenCalledTimes(1)
   })
 })
@@ -42,4 +44,9 @@ describe('useDebounce()', () => {
 function mockSetTimeout() {
   jest.useFakeTimers()
   jest.spyOn(global, 'setTimeout')
+}
+
+function mockClearTimeout() {
+  jest.useFakeTimers()
+  jest.spyOn(global, 'clearTimeout')
 }
