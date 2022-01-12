b1bc5faa0f52fc3ec80c5c2efd9d8d08cebbea0d
Egehan Dülger <14878259+egehandulger@users.noreply.github.com>
2022-01-12 12:43:33 +0100

    Add tests for useEventListener hook

---
+++ lib/src/useEventListener/useEventListener.test.ts
@@ -0,0 +1,120 @@
+import { fireEvent } from '@testing-library/react'
+import { renderHook } from '@testing-library/react-hooks/native'
+
+import useEventListener from './useEventListener'
+
+declare global {
+  interface WindowEventMap {
+    'test-event': CustomEvent
+  }
+
+  interface HTMLElementEventMap {
+    'test-event': CustomEvent
+  }
+}
+
+const addEventListenerToWindow = jest.fn()
+window.addEventListener = addEventListenerToWindow
+
+const windowAddEventListenerSpy = jest.spyOn(window, 'addEventListener')
+const windowRemoveEventListenerSpy = jest.spyOn(window, 'removeEventListener')
+
+const ref = { current: document.createElement('div') }
+const refAddEventListenerSpy = jest.spyOn(ref.current, 'addEventListener')
+const refRemoveEventListenerSpy = jest.spyOn(ref.current, 'removeEventListener')
+
+describe('useEventListener()', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should bind the event listener to the window when element is not provided', () => {
+    const eventName = 'test-event'
+
+    renderHook(() => useEventListener(eventName, jest.fn()))
+
+    expect(windowAddEventListenerSpy).toHaveBeenCalledWith(
+      eventName,
+      expect.anything(),
+    )
+  })
+
+  it('should bind the event listener to the element when element is provided', () => {
+    const eventName = 'test-event'
+
+    renderHook(() => useEventListener(eventName, jest.fn(), ref))
+
+    expect(windowAddEventListenerSpy).not.toHaveBeenCalledWith(
+      eventName,
+      expect.anything(),
+    )
+
+    expect(refAddEventListenerSpy).toHaveBeenCalledWith(
+      eventName,
+      expect.anything(),
+    )
+  })
+
+  it('should unbind the event listener from the window after the hook is unmounted', () => {
+    const eventName = 'test-event'
+
+    const { unmount } = renderHook(() => useEventListener(eventName, jest.fn()))
+
+    expect(windowAddEventListenerSpy).toHaveBeenCalledWith(
+      eventName,
+      expect.anything(),
+    )
+
+    unmount()
+
+    expect(windowRemoveEventListenerSpy).toHaveBeenCalledWith(
+      eventName,
+      expect.anything(),
+    )
+  })
+
+  it('should unbind the event listener from the element after the hook is unmounted', () => {
+    const eventName = 'test-event'
+
+    const { unmount } = renderHook(() =>
+      useEventListener(eventName, jest.fn(), ref),
+    )
+
+    expect(refAddEventListenerSpy).toHaveBeenCalledWith(
+      eventName,
+      expect.anything(),
+    )
+
+    unmount()
+
+    expect(refRemoveEventListenerSpy).toHaveBeenCalledWith(
+      eventName,
+      expect.anything(),
+    )
+  })
+
+  it('should call the event listener handler when the event is triggered', () => {
+    const eventName = 'click'
+    const handler = jest.fn()
+
+    renderHook(() => useEventListener(eventName, handler, ref))
+
+    fireEvent.click(ref.current)
+
+    expect(handler).toHaveBeenCalledTimes(1)
+  })
+
+  it('should have the correct event type', () => {
+    const clickHandler = jest.fn()
+    const keydownHandler = jest.fn()
+
+    renderHook(() => useEventListener('click', clickHandler, ref))
+    renderHook(() => useEventListener('keydown', keydownHandler, ref))
+
+    fireEvent.click(ref.current)
+    fireEvent.keyDown(ref.current)
+
+    expect(clickHandler).toHaveBeenCalledWith(expect.any(MouseEvent))
+    expect(keydownHandler).toHaveBeenCalledWith(expect.any(KeyboardEvent))
+  })
+})
