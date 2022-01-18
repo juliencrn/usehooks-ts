87acd03dae84c15baf9bb70ecb8bde03d99c9a38
Pablion <36828324+Pablion@users.noreply.github.com>
2022-01-18 08:32:13 +0100

    test: no dep err

--- site/src/hooks-doc/useTernaryDarkMode/useTernaryDarkMode.demo.tsx
+++ site/src/hooks-doc/useTernaryDarkMode/useTernaryDarkMode.demo.tsx
@@ -3,7 +3,15 @@ import React from 'react'
 import { useTernaryDarkMode } from 'usehooks-ts'
 
 export default function Component() {
-  const [two] = useTernaryDarkMode()
+  const { isPaletteDarkMode, ternaryModeCode, togglePaletteDarkMode } =
+    usePaletteDarkMode()
 
-  return <div>Hello {two}</div>
+  return (
+    <div>
+      <p>Current theme: {isDarkMode ? 'dark' : 'light'}</p>
+      <button onClick={toggle}>Toggle</button>
+      <button onClick={enable}>Enable</button>
+      <button onClick={disable}>Disable</button>
+    </div>
+  )
 }
