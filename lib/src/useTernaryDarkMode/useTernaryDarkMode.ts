566c9b4ed1c9d2744ab26f08e34636a0ea63a121
Pablion <36828324+Pablion@users.noreply.github.com>
2022-01-20 19:03:29 +0100

    fix: fix bugs in PR#86

--- lib/src/useTernaryDarkMode/useTernaryDarkMode.ts
+++ lib/src/useTernaryDarkMode/useTernaryDarkMode.ts
@@ -21,48 +21,29 @@ interface UseTernaryDarkModeOutput {
 }
 
 function useTernaryDarkMode(): UseTernaryDarkModeOutput {
-  /* ======== HELPERS ======== */
-  function useDarkModeOS(): boolean {
-    return useMediaQuery(COLOR_SCHEME_QUERY)
-    // Prevents SSR issues, old test failed const mQuery = useMediaQuery(COLOR_SCHEME_QUERY);
-    // if (typeof window !== 'undefined') {
-    //   if (window.hasOwnProperty('matchMedia')) {
-    //     return window.matchMedia(COLOR_SCHEME_QUERY).matches
-    //   }
-    // }
-    // return !!defaultValue
-  }
-  /* ======== REACT ======== */
-  const [isDarkOS, setDarkOS] = useState<boolean>(useDarkModeOS())
+  const isDarkOS = useMediaQuery(COLOR_SCHEME_QUERY)
   const [ternaryDarkMode, setTernaryDarkMode] =
     useLocalStorage<TernaryDarkMode>('ternaryDarkMode', TernaryDarkMode.System)
-  const [isDarkMode, setDarkMode] = useState<boolean>(true)
+  const [isDarkMode, setDarkMode] = useState<boolean>(isDarkOS)
 
   // Update darkMode if os prefers changes
   useUpdateEffect(() => {
-    setDarkOS(isDarkOS)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    if (ternaryDarkMode === TernaryDarkMode.System) {
+      setDarkMode(isDarkOS)
+    }
   }, [isDarkOS])
-  // useEffect(() => {
-  //   const handler = () => setDarkOS(getPrefersDarkMode())
-  //   const matchMedia = window.matchMedia(COLOR_SCHEME_QUERY)
-  //   matchMedia.addEventListener('change', handler)
-  //   return () => {
-  //     matchMedia.removeEventListener('change', handler)
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [])
 
   useEffect(() => {
-    // for setIsPaletteDarkMode
-    if (ternaryDarkMode === 'dark') {
-      setDarkMode(true)
-    } else if (ternaryDarkMode === 'system') {
-      setDarkMode(isDarkOS)
-    } else if (ternaryDarkMode === 'light') {
-      setDarkMode(false)
-    } else {
-      console.error('ERR reading palette mode')
+    switch (ternaryDarkMode) {
+      case TernaryDarkMode.Light:
+        setDarkMode(false)
+        break
+      case TernaryDarkMode.System:
+        setDarkMode(isDarkOS)
+        break
+      case TernaryDarkMode.Dark:
+        setDarkMode(true)
+        break
     }
   }, [ternaryDarkMode, isDarkOS])
 
