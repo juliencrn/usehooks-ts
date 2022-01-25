da06b5c6f16fcc1e363c97a706de50e293aa127d
Pablion <36828324+Pablion@users.noreply.github.com>
2022-01-25 22:19:39 +0100

    feat: add 'dispatchTernaryDarkMode' btn to demo

--- site/src/hooks-doc/useTernaryDarkMode/useTernaryDarkMode.demo.tsx
+++ site/src/hooks-doc/useTernaryDarkMode/useTernaryDarkMode.demo.tsx
@@ -3,19 +3,38 @@ import React from 'react'
 import { useTernaryDarkMode } from 'usehooks-ts'
 
 export default function Component() {
-  const { isDarkMode, ternaryDarkMode, toggleTernaryDarkMode } =
-    useTernaryDarkMode()
+  const {
+    isDarkMode,
+    ternaryDarkMode,
+    dispatchTernaryDarkMode,
+    toggleTernaryDarkMode,
+  } = useTernaryDarkMode()
+  type TernaryDarkMode = typeof ternaryDarkMode
 
   return (
     <div>
       <p>Current theme: {isDarkMode ? 'dark' : 'light'}</p>
       <p>ternaryMode: {ternaryDarkMode}</p>
-      <button onClick={toggleTernaryDarkMode}>
-        Toggle from {ternaryDarkMode}
-      </button>
       <p>
-        enum strict equal test:{' '}
-        {ternaryDarkMode === 'system' ? 'using System' : 'not using System'}
+        Toggle between three modes
+        <button onClick={toggleTernaryDarkMode}>
+          Toggle from {ternaryDarkMode}
+        </button>
+      </p>
+      <p>
+        Select a mode
+        <br />
+        <select
+          name="select-ternaryDarkMode"
+          onChange={ev =>
+            dispatchTernaryDarkMode(ev.target.value as TernaryDarkMode)
+          }
+          value={ternaryDarkMode}
+        >
+          <option value="light">light</option>
+          <option value="system">system</option>
+          <option value="dark">dark</option>
+        </select>
       </p>
     </div>
   )
