87e34a9fd834d445d3d3e09056b7f8adc194a7aa
Ajay Raja <92772740+AjayTheWizard@users.noreply.github.com>
2022-01-25 07:56:26 +0530

    Create useFormValue.ts

---
+++ lib/src/useFormValue/useFormValue.ts
@@ -0,0 +1,29 @@
+const useForm = (formRef: React.MutableRefObject<HTMLFormElement>) => {
+  interface valueType {
+    [key: string]: string;
+  }
+  let value: valueType = {};
+  if (typeof formRef.current !== "undefined") {
+    const formChildrens = formRef.current.children;
+    Object.keys(formChildrens).forEach((index: string) => {
+      const AnyElement = formChildrens[parseInt(index)];
+      if (AnyElement.tagName === "INPUT") {
+        const inputElement = formChildrens[parseInt(index)] as HTMLInputElement;
+        if (
+          inputElement.type === "text" || "datetime-local" ||
+          "color" || "date" || "email" || "month" || "number" ||
+          "password" || "tel" || "time" || "url" || "week")
+           {
+          value[inputElement.name] = inputElement.value;
+        }
+      }
+      if (AnyElement.tagName === "SELECT") {
+        const SelectElement = AnyElement as HTMLSelectElement;
+        value[SelectElement.name] = SelectElement.value;
+       }
+    });
+  }
+  return value;
+};
+
+export default useForm;
