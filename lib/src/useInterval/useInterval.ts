121b6541962dfd3ad468403703a69224aa665397
Julien <juliencaron@protonmail.com>
2022-02-20 23:59:34 +0100

    feat: replace useLayoutEffect by its isomorphic version

--- lib/src/useInterval/useInterval.ts
+++ lib/src/useInterval/useInterval.ts
@@ -1,10 +1,13 @@
-import { useEffect, useLayoutEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
+
+// See: https://usehooks-ts.com/react-hook/use-isomorphic-layout-effect
+import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect'
 
 function useInterval(callback: () => void, delay: number | null) {
   const savedCallback = useRef(callback)
 
   // Remember the latest callback if it changes.
-  useLayoutEffect(() => {
+  useIsomorphicLayoutEffect(() => {
     savedCallback.current = callback
   }, [callback])
 
