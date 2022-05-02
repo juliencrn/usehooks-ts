fb9a009f47a7149bc478c41a3edb24100615ed9e
Lucas Gomes <3GOMESz@gmail.com>
2022-05-02 10:09:11 -0300

    modify useEffectOnce fix #143

--- lib/src/useEffectOnce/useEffectOnce.ts
+++ lib/src/useEffectOnce/useEffectOnce.ts
@@ -1,8 +1,32 @@
-import { EffectCallback, useEffect } from 'react'
+import { EffectCallback, useEffect, useRef } from 'react'
 
 function useEffectOnce(effect: EffectCallback) {
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(effect, [])
+  const destroyFunc = useRef<void | any>();
+  const calledOnce = useRef(false);
+  const renderAfterCalled = useRef(false);
+
+  if (calledOnce.current) {
+    renderAfterCalled.current = true;
+  }
+
+  useEffect(() => {
+    if (calledOnce.current) {
+      return;
+    }
+
+    calledOnce.current = true;
+    destroyFunc.current = effect();
+
+    return () => {
+      if (!renderAfterCalled.current) {
+        return;
+      }
+
+      if (destroyFunc.current) {
+        destroyFunc.current();
+      }
+    };
+  }, []);
 }
 
 export default useEffectOnce
