94729e3aed374f81271ee42127362c3b8adabe6e
Jack Herrington <jherr@pobox.com>
2022-05-15 09:24:39 -0700

    Reverting useEffectOnce hook change

--- lib/src/useEffectOnce/useEffectOnce.ts
+++ lib/src/useEffectOnce/useEffectOnce.ts
@@ -1,34 +1,8 @@
-import { EffectCallback, useEffect, useRef } from 'react'
+import { EffectCallback, useEffect } from 'react'
 
 function useEffectOnce(effect: EffectCallback) {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const destroyFunc = useRef<void | any>()
-  const calledOnce = useRef(false)
-  const renderAfterCalled = useRef(false)
-
-  if (calledOnce.current) {
-    renderAfterCalled.current = true
-  }
-
-  useEffect(() => {
-    if (calledOnce.current) {
-      return
-    }
-
-    calledOnce.current = true
-    destroyFunc.current = effect()
-
-    return () => {
-      if (!renderAfterCalled.current) {
-        return
-      }
-
-      if (destroyFunc.current) {
-        destroyFunc.current()
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(effect, [])
 }
 
 export default useEffectOnce
