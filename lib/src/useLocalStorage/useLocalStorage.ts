6342ee5d92ae935292522e99a5dc44ee726928ca
Bogdan Ailincai <93596663+bogdanailincaipnt@users.noreply.github.com>
2022-05-16 11:19:30 +0300

    useLocalStorage: ignore unrelated 'storage' events
    Do not call `handleStorageChange` for storage events whose `key` does not match our key (this also fixes some perf problems that can occur because of this - handleStorageChange was being called too many times).

--- lib/src/useLocalStorage/useLocalStorage.ts
+++ lib/src/useLocalStorage/useLocalStorage.ts
@@ -79,9 +79,15 @@ function useLocalStorage<T>(key: string, initialValue: T): [T, SetValue<T>] {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  const handleStorageChange = useCallback(() => {
-    setStoredValue(readValue())
-  }, [readValue])
+  const handleStorageChange = useCallback(
+    (event: StorageEvent | CustomEvent) => {
+      if ((event as StorageEvent)?.key && (event as StorageEvent).key !== key) {
+        return
+      }
+      setStoredValue(readValue())
+    },
+    [key, readValue]
+  )
 
   // this only works for other documents, not the current one
   useEventListener('storage', handleStorageChange)
