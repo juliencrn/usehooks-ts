c3f94868b4ba8856a4e0ab36b05d32d88749d78b
Julien <juliencaron@protonmail.com>
2022-05-30 14:24:44 +0200

    refactor: re-organize tests

--- lib/src/useCountdown/useCountdown.test.ts
+++ lib/src/useCountdown/useCountdown.test.ts
@@ -2,43 +2,23 @@
 
 import { act, renderHook } from '@testing-library/react-hooks/native'
 
-import useCountdown, { DEPRECATED_WARN } from './useCountdown'
+import useCountdown from './useCountdown'
 
 jest.useFakeTimers()
 
-describe('deprecated useCountdown()', () => {
-  // deprecating.
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const packageJsonVersion = require('../../package.json').version
-  if (packageJsonVersion.startsWith('3.')) {
-    console.error(
-      'Skipping useCountdown test for version 3.x, please remove this test.',
-    )
-    return
-  }
-  test('should show deprecate warning', () => {
-    console.warn = jest.fn()
-    renderHook(() => {
-      useCountdown({ seconds: 60, interval: 500, isIncrement: false })
+describe('useCountdown()', () => {
+  describe('depreciated useCountdown()', () => {
+    test('should initialize', () => {
+      const { result } = renderHook(() =>
+        useCountdown({ seconds: 60, interval: 500, isIncrement: false }),
+      )
+
+      expect(result.current[0]).toBe(60)
+      expect(typeof result.current[1].start).toBe('function')
+      expect(typeof result.current[1].stop).toBe('function')
+      expect(typeof result.current[1].reset).toBe('function')
     })
-    expect(console.warn).not.toHaveBeenCalledWith('some random string')
-    // expect(console.warn.mock.calls[0][0]).toBe(DEPRECATED_WARN)
-    // HELP! I cant type this `.mock`
-    expect(console.warn).toHaveBeenCalledWith(DEPRECATED_WARN)
-  })
 
-  test('should use countdown', () => {
-    const { result } = renderHook(() =>
-      useCountdown({ seconds: 60, interval: 500, isIncrement: false }),
-    )
-
-    expect(result.current[0]).toBe(60)
-    expect(typeof result.current[1].start).toBe('function')
-    expect(typeof result.current[1].stop).toBe('function')
-    expect(typeof result.current[1].reset).toBe('function')
-  })
-
-  describe('start', () => {
     test('should increment count', () => {
       const { result } = renderHook(() =>
         useCountdown({ seconds: 60, interval: 500, isIncrement: true }),
@@ -49,7 +29,7 @@ describe('deprecated useCountdown()', () => {
         jest.advanceTimersByTime(1000)
       })
 
-      expect(result.current[0]).toBeGreaterThan(60)
+      expect(result.current[0]).toBe(62)
     })
 
     test('should decrement count', () => {
@@ -62,11 +42,9 @@ describe('deprecated useCountdown()', () => {
         jest.advanceTimersByTime(1000)
       })
 
-      expect(result.current[0]).toBeLessThan(60)
+      expect(result.current[0]).toBe(58)
     })
-  })
 
-  describe('stop', () => {
     test('should stop countdown', () => {
       const { result } = renderHook(() =>
         useCountdown({ seconds: 60, interval: 500 }),
@@ -79,10 +57,14 @@ describe('deprecated useCountdown()', () => {
       })
       act(result.current[1].stop)
       expect(result.current[0]).toBe(58)
-    })
-  })
 
-  describe('reset', () => {
+      act(() => {
+        jest.advanceTimersByTime(1000)
+      })
+
+      expect(result.current[0]).toBe(58)
+    })
+
     test('should reset count', () => {
       const { result } = renderHook(() =>
         useCountdown({ seconds: 60, interval: 500 }),
@@ -99,131 +81,155 @@ describe('deprecated useCountdown()', () => {
       expect(result.current[0]).toBe(60)
     })
   })
-})
 
-describe('useCountdown()', () => {
-  describe('call useCountdown', () => {
-    test('should return callable functions', () => {
-      const { result } = renderHook(() =>
-        useCountdown({ countStart: 60, intervalMs: 500, isIncrement: false }),
-      )
+  test('should return callable functions', () => {
+    const { result } = renderHook(() =>
+      useCountdown({ countStart: 60, intervalMs: 500, isIncrement: false }),
+    )
 
-      expect(result.current[0]).toBe(60)
-      expect(typeof result.current[1].startCountdown).toBe('function')
-      expect(typeof result.current[1].stopCountdown).toBe('function')
-      expect(typeof result.current[1].resetCountdown).toBe('function')
-    })
-    test('should accept only countStart', () => {
-      const { result } = renderHook(() => useCountdown({ countStart: 60 }))
-
-      expect(result.current[0]).toBe(60)
-      expect(typeof result.current[1].startCountdown).toBe('function')
-      expect(typeof result.current[1].stopCountdown).toBe('function')
-      expect(typeof result.current[1].resetCountdown).toBe('function')
-    })
-    test('should accept only countStart, intervalMs', () => {
-      const { result } = renderHook(() =>
-        useCountdown({ countStart: 60, intervalMs: 500 }),
-      )
-
-      expect(result.current[0]).toBe(60)
-      expect(typeof result.current[1].startCountdown).toBe('function')
-      expect(typeof result.current[1].stopCountdown).toBe('function')
-      expect(typeof result.current[1].resetCountdown).toBe('function')
-    })
-    test('should accept only countStart, intervalMs, countStop', () => {
-      const { result } = renderHook(() =>
-        useCountdown({ countStart: 60, intervalMs: 500, countStop: 0 }),
-      )
-
-      expect(result.current[0]).toBe(60)
-      expect(typeof result.current[1].startCountdown).toBe('function')
-      expect(typeof result.current[1].stopCountdown).toBe('function')
-      expect(typeof result.current[1].resetCountdown).toBe('function')
-    })
+    expect(result.current[0]).toBe(60)
+    expect(typeof result.current[1].startCountdown).toBe('function')
+    expect(typeof result.current[1].stopCountdown).toBe('function')
+    expect(typeof result.current[1].resetCountdown).toBe('function')
   })
 
-  describe('startCountdown', () => {
-    test('should increment count', () => {
-      const { result } = renderHook(() =>
-        useCountdown({
-          countStart: 60,
-          intervalMs: 1000,
-          isIncrement: true,
-          countStop: 0,
-        }),
-      )
+  test('should accept countStart', () => {
+    const { result } = renderHook(() => useCountdown({ countStart: 30 }))
 
-      act(result.current[1].startCountdown)
-      act(() => {
-        jest.advanceTimersByTime(1000)
-      })
-
-      expect(result.current[0]).toBe(61)
-    })
-
-    test('should decrement count', () => {
-      const { result } = renderHook(() => useCountdown({ countStart: 60 }))
-      // interval = 1sec by default.
-      act(result.current[1].startCountdown)
-      act(() => {
-        jest.advanceTimersByTime(1000)
-      })
-
-      expect(result.current[0]).toBeLessThan(60)
-    })
-
-    test('should stop at countStop', () => {
-      const { result } = renderHook(() =>
-        useCountdown({ countStart: 60, intervalMs: 1000, countStop: 0 }),
-      )
-      act(result.current[1].startCountdown)
-      const PASSED_TIME_SEC = 60
-      act(() => {
-        jest.advanceTimersByTime(PASSED_TIME_SEC * 1000)
-      })
-      expect(result.current[0]).toBe(60 - PASSED_TIME_SEC)
-      act(() => {
-        jest.advanceTimersByTime(60_000)
-      })
-      expect(result.current[0]).toBe(0)
-    })
+    expect(result.current[0]).toBe(30)
+    expect(typeof result.current[1].startCountdown).toBe('function')
+    expect(typeof result.current[1].stopCountdown).toBe('function')
+    expect(typeof result.current[1].resetCountdown).toBe('function')
   })
 
-  describe('stopCountdown', () => {
-    test('should stop countdown', () => {
-      const { result } = renderHook(() =>
-        useCountdown({ countStart: 60, intervalMs: 1000 }),
-      )
+  test('should accept intervalMs', () => {
+    const { result } = renderHook(() =>
+      useCountdown({ countStart: 60, intervalMs: 500 }),
+    )
 
-      expect(result.current[0]).toBe(60)
-      act(result.current[1].startCountdown)
-      act(() => {
-        jest.advanceTimersByTime(2000)
-      })
-      act(result.current[1].stopCountdown)
-      act(() => {
-        jest.advanceTimersByTime(3000)
-      })
-      expect(result.current[0]).toBe(58)
+    expect(result.current[0]).toBe(60)
+    expect(typeof result.current[1].startCountdown).toBe('function')
+    expect(typeof result.current[1].stopCountdown).toBe('function')
+    expect(typeof result.current[1].resetCountdown).toBe('function')
+
+    act(result.current[1].startCountdown)
+    act(() => {
+      jest.advanceTimersByTime(500)
     })
+
+    expect(result.current[0]).toBe(59)
   })
 
-  describe('resetCountdown', () => {
-    test('should reset count', () => {
-      const { result } = renderHook(() =>
-        useCountdown({ countStart: 60, intervalMs: 1000 }),
-      )
+  test('should stop at countStop (default: 0)', () => {
+    const { result } = renderHook(() =>
+      useCountdown({ countStart: 60, intervalMs: 1000 }),
+    )
 
-      act(result.current[1].startCountdown)
-      act(() => {
-        jest.advanceTimersByTime(1000)
-      })
-      act(result.current[1].stopCountdown)
-      expect(result.current[0]).toBeLessThan(60)
+    expect(result.current[0]).toBe(60)
+    expect(typeof result.current[1].startCountdown).toBe('function')
+    expect(typeof result.current[1].stopCountdown).toBe('function')
+    expect(typeof result.current[1].resetCountdown).toBe('function')
 
-      act(result.current[1].resetCountdown)
-      expect(result.current[0]).toBe(60)
+    act(result.current[1].startCountdown)
+    act(() => {
+      jest.advanceTimersByTime(60 * 1000)
     })
+
+    expect(result.current[0]).toBe(0)
+
+    act(() => {
+      jest.advanceTimersByTime(1000)
+    })
+
+    expect(result.current[0]).toBe(0)
+  })
+
+  test('should stop at custom countStop', () => {
+    const { result } = renderHook(() =>
+      useCountdown({ countStart: 60, intervalMs: 1000, countStop: 30 }),
+    )
+
+    expect(result.current[0]).toBe(60)
+    expect(typeof result.current[1].startCountdown).toBe('function')
+    expect(typeof result.current[1].stopCountdown).toBe('function')
+    expect(typeof result.current[1].resetCountdown).toBe('function')
+
+    act(result.current[1].startCountdown)
+    act(() => {
+      jest.advanceTimersByTime(30 * 1000)
+    })
+
+    expect(result.current[0]).toBe(30)
+
+    act(() => {
+      jest.advanceTimersByTime(1000)
+    })
+
+    expect(result.current[0]).toBe(30)
+  })
+
+  test('should stop countdown', () => {
+    const { result } = renderHook(() =>
+      useCountdown({ countStart: 60, intervalMs: 1000 }),
+    )
+
+    expect(result.current[0]).toBe(60)
+    act(result.current[1].startCountdown)
+    act(() => {
+      jest.advanceTimersByTime(2000)
+    })
+
+    expect(result.current[0]).toBe(58)
+    act(result.current[1].stopCountdown)
+    act(() => {
+      jest.advanceTimersByTime(3000)
+    })
+    expect(result.current[0]).toBe(58)
+  })
+
+  test('should stop reversed countdown', () => {
+    const { result } = renderHook(() =>
+      useCountdown({
+        countStart: 10,
+        intervalMs: 1000,
+        countStop: 20,
+        isIncrement: true,
+      }),
+    )
+
+    expect(result.current[0]).toBe(10)
+    act(result.current[1].startCountdown)
+    act(() => {
+      jest.advanceTimersByTime(2 * 1000)
+    })
+
+    expect(result.current[0]).toBe(12)
+
+    act(() => {
+      jest.advanceTimersByTime(8 * 1000)
+    })
+    expect(result.current[0]).toBe(20)
+
+    act(() => {
+      jest.advanceTimersByTime(3 * 1000)
+    })
+
+    expect(result.current[0]).toBe(20)
+  })
+
+  test('should reset count', () => {
+    const { result } = renderHook(() =>
+      useCountdown({ countStart: 60, intervalMs: 1000 }),
+    )
+
+    act(result.current[1].startCountdown)
+    act(() => {
+      jest.advanceTimersByTime(1000)
+    })
+    act(result.current[1].stopCountdown)
+    expect(result.current[0]).toBeLessThan(60)
+
+    act(result.current[1].resetCountdown)
+    expect(result.current[0]).toBe(60)
   })
 })
