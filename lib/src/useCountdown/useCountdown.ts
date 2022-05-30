033f5f7a9034aa612cb50d9bbf8e5e51f9094190
Julien <juliencaron@protonmail.com>
2022-05-30 14:24:20 +0200

    fix: hook implementation

--- lib/src/useCountdown/useCountdown.ts
+++ lib/src/useCountdown/useCountdown.ts
@@ -1,8 +1,8 @@
 // TODO: example and test
 import { useCallback } from 'react'
 
-import { useBoolean } from '../useBoolean'
 // See: https://usehooks-ts.com/react-hook/use-boolean
+import { useBoolean } from '../useBoolean'
 // See: https://usehooks-ts.com/react-hook/use-counter
 import { useCounter } from '../useCounter'
 // See: https://usehooks-ts.com/react-hook/use-interval
@@ -33,25 +33,20 @@ interface CountdownControllers {
   resetCountdown: () => void
 }
 
-// exported for test
-export const DEPRECATED_WARN =
-  '[useCountdown:DEPRECATED] new interface is already available (see @tutorial), the old version will retire on usehooks-ts@3.'
-
 /**
  *
  * @param  {UseCountdownType} countdownOption
  * @param  {number} countdownOption.seconds the countdown's number, generally time seconds
  * @param  {number} countdownOption.interval the countdown's interval, milliseconds
  * @param  {?boolean} countdownOption.isIncrement false by default, determine the countdown is increment, otherwise is decrement
+ * @returns [counter, CountdownControllers]
  *
- * @returns number
- *
- * @deprecated new useCountdown interface is already available (see @tutorial), the old version will retire on usehooks-ts@3
- * @tutorial https://usehooks-ts.com/react-hook/use-countdown
+ * @deprecated new useCountdown interface is already available (see https://usehooks-ts.com/react-hook/use-countdown), the old version will retire on usehooks-ts@3
  */
 function useCountdown(
   countdownOption: UseCountdownType,
 ): [number, CountdownHelpers]
+
 /**
  * New interface with default value
  *
@@ -60,29 +55,30 @@ function useCountdown(
  * @param  {?number} countdownOption.countStop -  `0` by default, the countdown's stopping number. Pass `-Infinity` to decrease forever.
  * @param  {?number} countdownOption.intervalMs - `1000` by default, the countdown's interval, in milliseconds.
  * @param  {?boolean} countdownOption.isIncrement - `false` by default, true if the countdown is increment.
- * @returns Array of [number, CountdownControllers]
- * @returns `number`: counting number
- * @returns `CountdownControllers`: Object of three methods {startCountdown, stopCountdown, resetCountdown}
+ * @returns [counter, CountdownControllers]
  */
 function useCountdown(
   countdownOption: CountdownOption,
 ): [number, CountdownControllers]
+
 function useCountdown(
   countdownOption: UseCountdownType | CountdownOption,
 ): [number, CountdownHelpers | CountdownControllers] {
   /**
    * Use to determine the the API call is a deprecated version.
    */
-  // const { value: isDeprecated, setTrue: setDeprecated } = useBoolean(false)
   let isDeprecated = false
-  // handle overload
+
   let countStart,
     intervalMs,
     isIncrement: boolean | undefined,
     countStop: number | undefined
+
   if ('seconds' in countdownOption) {
-    console.warn(DEPRECATED_WARN)
-    // setDeprecated();
+    console.warn(
+      '[useCountdown:DEPRECATED] new interface is already available (see https://usehooks-ts.com/react-hook/use-countdown), the old version will retire on usehooks-ts@3.',
+    )
+
     isDeprecated = true
     countStart = countdownOption.seconds
     intervalMs = countdownOption.interval
@@ -95,8 +91,7 @@ function useCountdown(
   // default values
   intervalMs = intervalMs ?? 1000
   isIncrement = isIncrement ?? false
-  countStop = countStop ?? 0
-  isIncrement = isIncrement ?? false
+  countStop = (countStop ?? 0) as number
 
   const {
     count,
@@ -126,26 +121,20 @@ function useCountdown(
   }
 
   const countdownCallback = useCallback(() => {
-    if (countStop === undefined) {
-      if (!isDeprecated) {
-        console.error('new call with deprecated value')
-      }
-    } else if (count <= countStop) {
+    if (count === countStop) {
       stopCountdown()
-    } else {
-      isIncrement ? increment() : decrement()
+      return
     }
-  }, [
-    count,
-    countStop,
-    decrement,
-    increment,
-    isDeprecated,
-    isIncrement,
-    stopCountdown,
-  ])
+
+    if (isIncrement) {
+      increment()
+    } else {
+      decrement()
+    }
+  }, [count, countStop, decrement, increment, isIncrement, stopCountdown])
 
   useInterval(countdownCallback, isCountdownRunning ? intervalMs : null)
+
   return isDeprecated
     ? [
         count,
