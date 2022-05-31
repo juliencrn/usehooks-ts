52561fec1b605628087d9a38a4ba35ae26b37fc8
Julien <juliencaron@protonmail.com>
2022-05-31 12:36:59 +0200

    feat: add Document based event support to useEventListener

--- lib/src/useEventListener/useEventListener.ts
+++ lib/src/useEventListener/useEventListener.ts
@@ -3,10 +3,15 @@ import { RefObject, useEffect, useRef } from 'react'
 // See: https://usehooks-ts.com/react-hook/use-isomorphic-layout-effect
 import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect'
 
+// Window Event based useEventListener interface
 function useEventListener<K extends keyof WindowEventMap>(
   eventName: K,
   handler: (event: WindowEventMap[K]) => void,
+  element?: undefined,
+  options?: boolean | AddEventListenerOptions,
 ): void
+
+// Element Event based useEventListener interface
 function useEventListener<
   K extends keyof HTMLElementEventMap,
   T extends HTMLElement = HTMLDivElement,
@@ -14,6 +19,15 @@ function useEventListener<
   eventName: K,
   handler: (event: HTMLElementEventMap[K]) => void,
   element: RefObject<T>,
+  options?: boolean | AddEventListenerOptions,
+): void
+
+// Document Event based useEventListener interface
+function useEventListener<K extends keyof DocumentEventMap>(
+  eventName: K,
+  handler: (event: DocumentEventMap[K]) => void,
+  element: RefObject<Document>,
+  options?: boolean | AddEventListenerOptions,
 ): void
 
 function useEventListener<
@@ -26,6 +40,7 @@ function useEventListener<
     event: WindowEventMap[KW] | HTMLElementEventMap[KH] | Event,
   ) => void,
   element?: RefObject<T>,
+  options?: boolean | AddEventListenerOptions,
 ) {
   // Create a ref that stores handler
   const savedHandler = useRef(handler)
@@ -44,13 +59,13 @@ function useEventListener<
     // Create event listener that calls handler function stored in ref
     const eventListener: typeof handler = event => savedHandler.current(event)
 
-    targetElement.addEventListener(eventName, eventListener)
+    targetElement.addEventListener(eventName, eventListener, options)
 
     // Remove event listener on cleanup
     return () => {
       targetElement.removeEventListener(eventName, eventListener)
     }
-  }, [eventName, element])
+  }, [eventName, element, options])
 }
 
 export default useEventListener
--- site/src/hooks-doc/useEventListener/useEventListener.demo.tsx
+++ site/src/hooks-doc/useEventListener/useEventListener.demo.tsx
@@ -5,6 +5,7 @@ import { useEventListener } from 'usehooks-ts'
 export default function Component() {
   // Define button ref
   const buttonRef = useRef<HTMLButtonElement>(null)
+  const documentRef = useRef<Document>(document)
 
   const onScroll = (event: Event) => {
     console.log('window scrolled!', event)
@@ -14,8 +15,18 @@ export default function Component() {
     console.log('button clicked!', event)
   }
 
+  const onVisibilityChange = (event: Event) => {
+    console.log('doc visibility changed!', {
+      isVisible: !document.hidden,
+      event,
+    })
+  }
+
   // example with window based event
   useEventListener('scroll', onScroll)
+
+  // example with document based event
+  useEventListener('visibilitychange', onVisibilityChange, documentRef)
 
   // example with element based event
   useEventListener('click', onClick, buttonRef)
