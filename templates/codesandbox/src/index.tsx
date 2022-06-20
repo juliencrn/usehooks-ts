46eb3ac335c485137983d43e488afae917e5fb18
Julien <juliencaron@protonmail.com>
2022-06-20 13:56:47 +0200

    chore: update codesandbox index for react 18

--- templates/codesandbox/src/index.tsx
+++ templates/codesandbox/src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import './styles.css'
 import App from './App'
 
-const rootElement = document.getElementById('root')
-render(<App />, rootElement)
+const container = document.getElementById('root')
+const root = createRoot(container!)
+root.render(<App />)
