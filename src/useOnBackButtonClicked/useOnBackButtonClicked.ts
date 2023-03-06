a05bc7732672c106d8b56f440b80218a236d03d4
Matias <mati_d_pinto@hotmail.com>
2023-03-06 20:15:03 -0300

    useOnBackButtonClicked hook created

--- src/useOnBackButtonClicked/useOnBackButtonClicked.ts
+++ src/useOnBackButtonClicked/useOnBackButtonClicked.ts
@@ -1,8 +1,26 @@
-function useOnBackButtonClicked(): [number, () => number] {
-const value = 2
-const method = () => 2
+import { useCallback, useEffect } from 'react'
 
-return [value, method]
+interface useOnBackButtonClickedInterface {
+  callback: () => void
+  text?: string
+}
+
+const DEFAULT_TEXT = 'Estas seguro que deseas ir para atras?' // TODO: Poner en ingles
+
+function useOnBackButtonClicked({
+  callback,
+  text = DEFAULT_TEXT,
+}: useOnBackButtonClickedInterface) {
+  const handleBackButtonPressed = useCallback(async () => {
+    const retValue = window.confirm(text)
+    if (retValue) {
+      callback()
+    }
+  }, [callback, text])
+
+  useEffect(() => {
+    // TODO: Add listeners
+  }, [handleBackButtonPressed])
 }
 
 export default useOnBackButtonClicked
