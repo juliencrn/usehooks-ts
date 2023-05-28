c4e2ae5fcf0382fecd238396c9f42d0c9001ecdc
Julien <juliencaron@protonmail.com>
2023-05-28 03:39:48 +0200

    chore: move usehooks files to packages/usehooks-ts

---
+++ packages/usehooks-ts/jest.config.js
@@ -0,0 +1,18 @@
+const ignoreDirs = ['dist/', 'node_modules/', 'scripts/']
+
+/** @type {Config.InitialOptions} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  verbose: false,
+  silent: true,
+  transform: {
+    '^.+\\.[t]sx?$': `ts-jest`,
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  testPathIgnorePatterns: ignoreDirs,
+  coveragePathIgnorePatterns: ignoreDirs,
+  coverageDirectory: '<rootDir>/coverage/',
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+}
