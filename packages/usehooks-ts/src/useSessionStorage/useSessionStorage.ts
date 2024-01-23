d2665b05630de9971d7dc18af5db9e270d610a8f
Julien <juliencaron@protonmail.com>
2024-01-23 11:17:29 +0100

    🚧 Prefer console.warn over consle.log

--- packages/usehooks-ts/src/useSessionStorage/useSessionStorage.ts
+++ packages/usehooks-ts/src/useSessionStorage/useSessionStorage.ts
@@ -100,7 +100,7 @@ function parseJSON<T>(value: string | null): T | undefined {
   try {
     return value === 'undefined' ? undefined : JSON.parse(value ?? '')
   } catch {
-    console.log('parsing error on', { value })
+    console.warn('parsing error on', { value })
     return undefined
   }
 }
