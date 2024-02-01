cb6eb5c9571dc783f9c371181a12763e40bbd403
Ladislas Dellinger <111739019+ladislasdellinger@users.noreply.github.com>
2024-02-01 02:26:24 +0100

    ✨ feat: add an option to reset to default document title on unmount for useDocumentTitle (#346)
    * ✨ feat: 345 add an option to reset to default document title on unmount for useDocumentTitle
    
    * ♻️ Improve a bit some details and fix SSR issue
    
    - renamed `prevailOnUnmount` to  `preserveTitleOnUnmount`
    - moved it in an options object
    - prefer useUnmount for un-mount effect
    - `window` in `useRef` is not SSR-safe
    
    ---------
    
    Co-authored-by: Julien <juliencaron@protonmail.com>

--- packages/usehooks-ts/src/useDocumentTitle/useDocumentTitle.test.ts
+++ packages/usehooks-ts/src/useDocumentTitle/useDocumentTitle.test.ts
@@ -9,4 +9,24 @@ describe('useDocumentTitle()', () => {
     })
     expect(window.document.title).toEqual('foo')
   })
+
+  it('should unset title on unmount with `preserveTitleOnUnmount` options to `false`', () => {
+    window.document.title = 'initial'
+    const { unmount } = renderHook(() => {
+      useDocumentTitle('updated', { preserveTitleOnUnmount: false })
+    })
+    expect(window.document.title).toEqual('updated')
+    unmount()
+    expect(window.document.title).toEqual('initial')
+  })
+
+  it("shouldn't unset title on unmount with `preserveTitleOnUnmount` options to `true` (default)", () => {
+    window.document.title = 'initial'
+    const { unmount } = renderHook(() => {
+      useDocumentTitle('updated')
+    })
+    expect(window.document.title).toEqual('updated')
+    unmount()
+    expect(window.document.title).toEqual('updated')
+  })
 })
--- packages/usehooks-ts/src/useDocumentTitle/useDocumentTitle.ts
+++ packages/usehooks-ts/src/useDocumentTitle/useDocumentTitle.ts
@@ -1,14 +1,36 @@
+import { useRef } from 'react'
+
 import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect'
+import { useUnmount } from '../useUnmount'
+
+type Options = {
+  preserveTitleOnUnmount?: boolean
+}
 
 /**
  * A hook to set the document title.
  * @param {string} title - The title to set.
+ * @param {?Options} [options] - The options.
+ * @param {?boolean} [options.preserveTitleOnUnmount] - Whether to keep the title after unmounting the component (default is `true`).
  * @see [Documentation](https://usehooks-ts.com/react-hook/use-document-title)
  * @example
  * useDocumentTitle('My new title');
  */
-export function useDocumentTitle(title: string) {
+export function useDocumentTitle(title: string, options: Options = {}): void {
+  const { preserveTitleOnUnmount = true } = options
+  const defaultTitle = useRef<string | null>(null)
+
+  useIsomorphicLayoutEffect(() => {
+    defaultTitle.current = window.document.title
+  }, [])
+
   useIsomorphicLayoutEffect(() => {
     window.document.title = title
   }, [title])
+
+  useUnmount(() => {
+    if (!preserveTitleOnUnmount && defaultTitle.current) {
+      window.document.title = defaultTitle.current
+    }
+  })
 }
