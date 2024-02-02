366d2b5c076dea41ba1caf6e7268aeea18a99447
Julien <juliencaron@protonmail.com>
2024-02-03 00:06:02 +0100

    ✨ Add touch event support

--- packages/usehooks-ts/src/useOnClickOutside/useOnClickOutside.ts
+++ packages/usehooks-ts/src/useOnClickOutside/useOnClickOutside.ts
@@ -2,12 +2,14 @@ import type { RefObject } from 'react'
 
 import { useEventListener } from '../useEventListener'
 
+type EventType = 'mousedown' | 'mouseup' | 'touchstart' | 'touchend'
+
 /**
  * Custom hook for handling clicks outside a specified element.
  * @template T - The type of the element's reference.
  * @param {RefObject<T>} ref - The React ref object representing the element to watch for outside clicks.
- * @param {(event: MouseEvent) => void} handler - The callback function to be executed when a click outside the element occurs.
- * @param {'mousedown' | 'mouseup'} [mouseEvent] - The mouse event type to listen for (optional, default is 'mousedown').
+ * @param {(event: MouseEvent | TouchEvent) => void} handler - The callback function to be executed when a click outside the element occurs.
+ * @param {EventType} [eventType] - The mouse event type to listen for (optional, default is 'mousedown').
  * @returns {void}
  * @see [Documentation](https://usehooks-ts.com/react-hook/use-on-click-outside)
  * @example
@@ -18,12 +20,10 @@ import { useEventListener } from '../useEventListener'
  */
 export function useOnClickOutside<T extends HTMLElement = HTMLElement>(
   ref: RefObject<T>,
-  handler: (event: MouseEvent) => void,
-  mouseEvent: 'mousedown' | 'mouseup' = 'mousedown',
+  handler: (event: MouseEvent | TouchEvent) => void,
+  eventType: EventType = 'mousedown',
 ): void {
-  useEventListener(mouseEvent, event => {
-    const el = ref.current
-
+  useEventListener(eventType, event => {
     const target = event.target as Node
 
     // Do nothing if clicking ref's element or descendent elements or not connected element with document
