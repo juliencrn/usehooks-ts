87a514103218b58070a5fdae690e5a07d5baace0
Hooriza <hooriza@gmail.com>
2024-02-03 09:36:43 +0900

    🐛 Fix and improve useOnClickOutside (#374 by @hooriza)
    * fix: to not call handler when disconnected target's event occurs
    
    * ✨ Add touch event support
    
    * ✨ Add multi refs support
    
    * ✅ Add unit tests
    
    * 🔖 Add changeset
    
    ---------
    
    Co-authored-by: Julien <juliencaron@protonmail.com>

---
+++ .changeset/eight-buses-rush.md
@@ -0,0 +1,9 @@
+---
+'usehooks-ts': minor
+---
+
+Improve `useOnClickOutside`:
+
+- Prevent handling callback when clicking on a not connected element (#374 by @hooriza)
+- Add support to accept multiple references
+- Add support for touch events in addition to mouse events
--- packages/usehooks-ts/src/useOnClickOutside/useOnClickOutside.ts
+++ packages/usehooks-ts/src/useOnClickOutside/useOnClickOutside.ts
@@ -2,33 +2,41 @@ import type { RefObject } from 'react'
 
 import { useEventListener } from '../useEventListener'
 
+type EventType = 'mousedown' | 'mouseup' | 'touchstart' | 'touchend'
+
 /**
  * Custom hook for handling clicks outside a specified element.
  * @template T - The type of the element's reference.
- * @param {RefObject<T>} ref - The React ref object representing the element to watch for outside clicks.
- * @param {(event: MouseEvent) => void} handler - The callback function to be executed when a click outside the element occurs.
- * @param {'mousedown' | 'mouseup'} [mouseEvent] - The mouse event type to listen for (optional, default is 'mousedown').
+ * @param {RefObject<T> | RefObject<T>[]} ref - The React ref object(s) representing the element(s) to watch for outside clicks.
+ * @param {(event: MouseEvent | TouchEvent) => void} handler - The callback function to be executed when a click outside the element occurs.
+ * @param {EventType} [eventType] - The mouse event type to listen for (optional, default is 'mousedown').
  * @returns {void}
  * @see [Documentation](https://usehooks-ts.com/react-hook/use-on-click-outside)
  * @example
  * const containerRef = useRef(null);
- * useOnClickOutside(containerRef, () => {
+ * useOnClickOutside([containerRef], () => {
  *   // Handle clicks outside the container.
  * });
  */
 export function useOnClickOutside<T extends HTMLElement = HTMLElement>(
-  ref: RefObject<T>,
-  handler: (event: MouseEvent) => void,
-  mouseEvent: 'mousedown' | 'mouseup' = 'mousedown',
+  ref: RefObject<T> | RefObject<T>[],
+  handler: (event: MouseEvent | TouchEvent) => void,
+  eventType: EventType = 'mousedown',
 ): void {
-  useEventListener(mouseEvent, event => {
-    const el = ref.current
+  useEventListener(eventType, event => {
+    const target = event.target as Node
 
-    // Do nothing if clicking ref's element or descendent elements
-    if (!el || el.contains(event.target as Node)) {
+    // Do nothing if the target is not connected element with document
+    if (!target || !target.isConnected) {
       return
     }
 
-    handler(event)
+    const isOutside = Array.isArray(ref)
+      ? ref.every(r => r.current && !r.current.contains(target))
+      : ref.current && !ref.current.contains(target)
+
+    if (isOutside) {
+      handler(event)
+    }
   })
 }
