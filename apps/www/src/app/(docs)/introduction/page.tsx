ebf4c9987bee06c5c35bd2cf2de8b7dca67ce549
BlankParticle <blankparticle@gmail.com>
2024-02-05 23:51:13 +0530

    feat: CommandCopy component and initial words

--- apps/www/src/app/(docs)/introduction/page.tsx
+++ apps/www/src/app/(docs)/introduction/page.tsx
@@ -1,6 +1,7 @@
+import { CommandCopy } from '@/components/command-copy'
 import { DocsPageHeader } from '@/components/docs-page-header'
-import { Mdx } from '@/components/remote-mdx'
-import { H2 } from '@/components/ui/components'
+import { components } from '@/components/ui/components'
+import { siteConfig } from '@/config/site'
 
 export default async function IntroductionPage() {
   return (
@@ -11,8 +12,28 @@ export default async function IntroductionPage() {
           className="scroll-m-20"
           heading={'Getting started'}
         />
-        <H2>Install</H2>
-        <Mdx source={Buffer.from('```bash\nnpm i usehooks-ts\n```')} />
+
+        <components.h2>Install</components.h2>
+        <components.p>
+          In order to get started with using{' '}
+          <components.a href={siteConfig.links.github}>
+            useHooks(🔥).ts
+          </components.a>
+          , Install{' '}
+          <components.code>
+            <components.a href={siteConfig.links.npm}>usehooks-ts</components.a>
+          </components.code>{' '}
+          using your preferred package manager.
+        </components.p>
+        <CommandCopy
+          className="my-2"
+          command={{
+            npm: 'npm install usehooks-ts',
+            pnpm: 'pnpm add usehooks-ts',
+            yarn: 'yarn add usehooks-ts',
+            bun: 'bun add usehooks-ts',
+          }}
+        />
       </div>
     </main>
   )
