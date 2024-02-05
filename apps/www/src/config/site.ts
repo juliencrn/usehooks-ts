a82837761d730f846f7915d0c29d5e8b3c806cda
Julien <juliencaron@protonmail.com>
2024-02-05 11:08:19 +0100

    Revert "🔥 chore: add og image for homepage"
    This reverts commit f9438d22fdb92d5e7fe54bd5fe7eeb40d00a59db.

--- apps/www/src/config/site.ts
+++ apps/www/src/config/site.ts
@@ -1,12 +1,13 @@
 import type { SiteConfig } from '@/types'
 
-export const siteConfig = {
+export const siteConfig: SiteConfig = {
   name: 'usehooks-ts',
   description: 'React hook library, ready to use, written in Typescript.',
   url: 'https://usehooks-ts.com',
-  ogImage: 'https://usehooks-ts.com/og-image.png',
+  ogImage:
+    'https://via.placeholder.com/1200x630.png/007ACC/fff/?text=usehooks-ts',
   links: {
     github: 'https://github.com/juliencrn/usehooks-ts',
     npm: 'https://www.npmjs.com/package/usehooks-ts',
   },
-} satisfies SiteConfig
+}
