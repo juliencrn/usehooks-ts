e9031240359a8fe08946bf561ea45fd2c0c50e35
BlankParticle <blankparticle@gmail.com>
2024-02-08 17:24:43 +0530

    chore: add introduction section to the docs

--- apps/www/src/app/(docs)/introduction/page.tsx
+++ apps/www/src/app/(docs)/introduction/page.tsx
@@ -13,6 +13,25 @@ export default async function IntroductionPage() {
           heading={'Getting started'}
         />
 
+        <components.h2>Introduction</components.h2>
+        <components.h3 className="text-muted-foreground my-0 font-normal">
+          React hook library, ready to use, written in Typescript.
+        </components.h3>
+        <components.p>
+          <span className="font-bold">useHooks(🔥).ts </span>
+          is a React hooks library, written in Typescript. It provides a set of
+          hooks that enables you to build your React applications faster. The
+          hooks are built upon the principles of{' '}
+          <span className="font-semibold">DRY</span> (Don&apos;t Repeat
+          Yourself). There are hooks for most common use cases you might need.
+        </components.p>
+        <components.p>
+          The library is designed to be as minimal as possible. It is fully
+          treeshakable, meaning that you only import the hooks you need, and the
+          rest will be removed from your bundle making the cost of using this
+          library negligible. Most hooks are extensively tested and are being
+          used in Production environments.
+        </components.p>
         <components.h2>Install</components.h2>
         <components.p>
           In order to get started with using{' '}
