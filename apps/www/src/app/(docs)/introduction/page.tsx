4456e32fd538daecfd67f1f47857081a7147e1c4
Rahul Mishra <blankparticle@gmail.com>
2024-02-13 02:18:07 +0530

    📝 Improve docs introduction (#471)
    * feat: CommandCopy component and initial words
    
    * chore: add introduction section to the docs
    
    * 🔖 Added changeset
    
    * 📝 Little adjustment on the introduction doc page
    
    ---------
    
    Co-authored-by: Julien <juliencaron@protonmail.com>

--- apps/www/src/app/(docs)/introduction/page.tsx
+++ apps/www/src/app/(docs)/introduction/page.tsx
@@ -1,6 +1,7 @@
+import { CommandCopy } from '@/components/command-copy'
 import { DocsPageHeader } from '@/components/docs-page-header'
-import { Mdx } from '@/components/remote-mdx'
-import { H2 } from '@/components/ui/components'
+import { components } from '@/components/ui/components'
+import { siteConfig } from '@/config/site'
 
 export default async function IntroductionPage() {
   return (
@@ -11,8 +12,39 @@ export default async function IntroductionPage() {
           className="scroll-m-20"
           heading={'Getting started'}
         />
-        <H2>Install</H2>
-        <Mdx source={Buffer.from('```bash\nnpm i usehooks-ts\n```')} />
+        <components.h2>Introduction</components.h2>
+        <components.p>
+          <span className="font-bold">useHooks(🔥).ts </span>
+          is a React hooks library, written in Typescript and easy to use. It
+          provides a set of hooks that enables you to build your React
+          applications faster. The hooks are built upon the principles of{' '}
+          <span className="font-semibold">DRY</span> (Don&apos;t Repeat
+          Yourself). There are hooks for most common use cases you might need.
+        </components.p>
+        <components.p>
+          The library is designed to be as minimal as possible. It is fully
+          tree-shakable (using the ESM version), meaning that you only import
+          the hooks you need, and the rest will be removed from your bundle
+          making the cost of using this library negligible. Most hooks are
+          extensively tested and are being used in production environments.
+        </components.p>
+        <components.h2>Install</components.h2>
+        <components.p>
+          Get started installing{' '}
+          <components.code>
+            <components.a href={siteConfig.links.npm}>usehooks-ts</components.a>
+          </components.code>{' '}
+          using your preferred package manager.
+        </components.p>
+        <CommandCopy
+          className="my-2"
+          command={{
+            npm: 'npm install usehooks-ts',
+            pnpm: 'pnpm add usehooks-ts',
+            yarn: 'yarn add usehooks-ts',
+            bun: 'bun add usehooks-ts',
+          }}
+        />
       </div>
     </main>
   )
---
+++ apps/www/src/components/command-copy.tsx
@@ -0,0 +1,96 @@
+'use client'
+
+import { useState } from 'react'
+
+import { Check, Copy } from 'lucide-react'
+import type { ComponentProps } from 'react'
+import { useCopyToClipboard } from 'usehooks-ts'
+
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+import { cn } from '@/lib/utils'
+
+interface CommandCopyProps extends ComponentProps<'code'> {
+  command: Record<string, string> | string
+  defaultCommand?: string
+}
+
+export function CommandCopy({
+  className,
+  command,
+  defaultCommand,
+  ...props
+}: CommandCopyProps) {
+  const [copiedStatus, setCopiedStatus] = useState(false)
+  const [, copy] = useCopyToClipboard()
+
+  const handleCopy = (text: string) => {
+    setCopiedStatus(true)
+    void copy(text)
+    setTimeout(() => {
+      setCopiedStatus(false)
+    }, 2000)
+  }
+  const renderedCommand =
+    typeof command === 'string'
+      ? command
+      : command[defaultCommand ?? Object.keys(command)[0]]
+
+  return (
+    <code
+      className={cn(
+        'rounded border p-2 gap-2 font-mono text-sm flex dark:bg-[#0d1117] bg-white items-center justify-between',
+        className,
+      )}
+      {...props}
+    >
+      <div className="px-2">
+        {renderedCommand.split(' ').map((arg, i) => (
+          <span
+            key={arg}
+            className={cn(i === 0 ? 'font-bold' : 'text-muted-foreground')}
+          >
+            {arg}{' '}
+          </span>
+        ))}
+      </div>
+      <DropdownMenu open={typeof command === 'string' ? false : undefined}>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => {
+              if (typeof command === 'string') {
+                handleCopy(renderedCommand)
+              }
+            }}
+          >
+            {copiedStatus ? (
+              <Check size={16} className="text-muted-foreground" />
+            ) : (
+              <Copy size={16} className="text-muted-foreground" />
+            )}
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          {command &&
+            Object.entries(command).map(([key, value]) => (
+              <DropdownMenuItem
+                key={key}
+                onClick={() => {
+                  handleCopy(value)
+                }}
+              >
+                {key}
+              </DropdownMenuItem>
+            ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </code>
+  )
+}
