db5c76de0d4aa70fc42657b99d3d00fa691b19b0
Julien <juliencaron@protonmail.com>
2024-02-12 21:39:14 +0100

    📝 Little adjustment on the introduction doc page

--- apps/www/src/app/(docs)/introduction/page.tsx
+++ apps/www/src/app/(docs)/introduction/page.tsx
@@ -12,33 +12,25 @@ export default async function IntroductionPage() {
           className="scroll-m-20"
           heading={'Getting started'}
         />
-
         <components.h2>Introduction</components.h2>
-        <components.h3 className="text-muted-foreground my-0 font-normal">
-          React hook library, ready to use, written in Typescript.
-        </components.h3>
         <components.p>
           <span className="font-bold">useHooks(🔥).ts </span>
-          is a React hooks library, written in Typescript. It provides a set of
-          hooks that enables you to build your React applications faster. The
-          hooks are built upon the principles of{' '}
+          is a React hooks library, written in Typescript and easy to use. It
+          provides a set of hooks that enables you to build your React
+          applications faster. The hooks are built upon the principles of{' '}
           <span className="font-semibold">DRY</span> (Don&apos;t Repeat
           Yourself). There are hooks for most common use cases you might need.
         </components.p>
         <components.p>
           The library is designed to be as minimal as possible. It is fully
-          treeshakable, meaning that you only import the hooks you need, and the
-          rest will be removed from your bundle making the cost of using this
-          library negligible. Most hooks are extensively tested and are being
-          used in Production environments.
+          tree-shakable (using the ESM version), meaning that you only import
+          the hooks you need, and the rest will be removed from your bundle
+          making the cost of using this library negligible. Most hooks are
+          extensively tested and are being used in production environments.
         </components.p>
         <components.h2>Install</components.h2>
         <components.p>
-          In order to get started with using{' '}
-          <components.a href={siteConfig.links.github}>
-            useHooks(🔥).ts
-          </components.a>
-          , Install{' '}
+          Get started installing{' '}
           <components.code>
             <components.a href={siteConfig.links.npm}>usehooks-ts</components.a>
           </components.code>{' '}
