6e1e13ff34351b339affc8b6b290a28114a84210
Kyrylo Melnychuk <kyrylo@soulandwolf.com.au>
2024-03-03 22:59:43 +1100

    Fix how reflow padding is calculated
    - Fixed padding when the body is the target
    - Fixed initial padding not being considered

--- packages/usehooks-ts/src/useScrollLock/useScrollLock.ts
+++ packages/usehooks-ts/src/useScrollLock/useScrollLock.ts
@@ -65,9 +65,18 @@ export function useScrollLock(
 
       // Prevent width reflow
       if (widthReflow) {
-        const scrollbarWidth =
-          target.current.offsetWidth - target.current.scrollWidth
-        target.current.style.paddingRight = `${scrollbarWidth}px`
+        // Use window inner width if body is the target as global scrollbar isn't part of the document
+        const offsetWidth =
+          target.current === document.body
+            ? window.innerWidth
+            : target.current.offsetWidth
+        // Get current computed padding right in pixels
+        const currentPaddingRight =
+          parseInt(window.getComputedStyle(target.current).paddingRight, 10) ||
+          0
+
+        const scrollbarWidth = offsetWidth - target.current.scrollWidth
+        target.current.style.paddingRight = `${scrollbarWidth + currentPaddingRight}px`
       }
 
       // Lock the scroll
