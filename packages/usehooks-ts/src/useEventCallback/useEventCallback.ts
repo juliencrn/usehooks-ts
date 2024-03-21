054bfbbc2b4577afe94e85506989842267c24558
Newbie012 <co.eliya2@gmail.com>
2024-03-21 10:09:03 +0200

    feat(useEventCallback): allow optional callback
    Updated the useEventCallback hook to accept an optional callback function. This improves developer experience by allowing more flexibility in the use of the hook.

--- packages/usehooks-ts/src/useEventCallback/useEventCallback.ts
+++ packages/usehooks-ts/src/useEventCallback/useEventCallback.ts
@@ -19,7 +19,13 @@ import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect'
  */
 export function useEventCallback<Args extends unknown[], R>(
   fn: (...args: Args) => R,
-): (...args: Args) => R {
+): (...args: Args) => R
+export function useEventCallback<Args extends unknown[], R>(
+  fn: ((...args: Args) => R) | undefined,
+): ((...args: Args) => R) | undefined
+export function useEventCallback<Args extends unknown[], R>(
+  fn: ((...args: Args) => R) | undefined,
+): ((...args: Args) => R) | undefined {
   const ref = useRef<typeof fn>(() => {
     throw new Error('Cannot call an event handler while rendering.')
   })
@@ -28,5 +34,7 @@ export function useEventCallback<Args extends unknown[], R>(
     ref.current = fn
   }, [fn])
 
-  return useCallback((...args: Args) => ref.current(...args), [ref])
+  return useCallback((...args: Args) => ref.current?.(...args), [ref]) as (
+    ...args: Args
+  ) => R
 }
