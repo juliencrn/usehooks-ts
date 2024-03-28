d8797335cdd5aa813d77e4d959fed27b83b3f298
Julien <juliencaron@protonmail.com>
2024-03-28 14:07:32 +0100

    Add LD+JSON for SEO

--- apps/www/src/app/(docs)/react-hook/[slug]/page.tsx
+++ apps/www/src/app/(docs)/react-hook/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import type { Article, WithContext } from 'schema-dts'
 
 import { PageHeader } from '@/components/docs/page-header'
 import { Pager } from '@/components/docs/pager'
@@ -11,6 +12,18 @@ export const generateStaticParams = async () => {
   return hooks.map(hook => ({ slug: hook.slug }))
 }
 
+function stringifyDescription(description: string) {
+  return description.replace(/\[([^\]]+)\]\([^)]+\)/g, '$1').replace(/`/g, '')
+}
+
+function getPostUrl(slug: string) {
+  return `${siteConfig.url}/react-hook/${slug}`
+}
+
+function getImageUrl(name: string) {
+  return `https://via.placeholder.com/1200x630.png/007ACC/fff/?text=${name}`
+}
+
 export const generateMetadata = async (props: {
   params: { slug: string }
 }): Promise<Metadata> => {
@@ -21,11 +34,9 @@ export const generateMetadata = async (props: {
   }
 
   const title = hook.name
-  const description = hook.summary
-    .replace(/\[([^\]]+)\]\([^)]+\)/g, '$1')
-    .replace(/`/g, '')
-  const url = siteConfig.url + hook.path
-  const imageUrl = `https://via.placeholder.com/1200x630.png/007ACC/fff/?text=${title}`
+  const description = stringifyDescription(hook.summary)
+  const url = getPostUrl(hook.slug)
+  const imageUrl = getImageUrl(title)
   return {
     title,
     description,
@@ -62,30 +73,56 @@ export default async function HookPage({
     getHookList(),
   ])
 
+  const ldJson: WithContext<Article> = {
+    '@context': 'https://schema.org',
+    '@type': 'Article',
+    headline: frontmatter.name,
+    description: stringifyDescription(frontmatter.summary),
+    url: getPostUrl(frontmatter.slug),
+    image: [getImageUrl(frontmatter.name)],
+    author: [
+      {
+        '@type': 'Organization',
+        name: 'usehooks-ts',
+        url: siteConfig.url,
+      },
+    ],
+  }
+
   return (
-    <main className="relative py-6 lg:gap-10 lg:py-10 xl:grid xl:grid-cols-[1fr_300px] xl:grid-rows-[auto_1fr_auto]">
-      <div className="mx-auto w-full min-w-0 grid">
-        <PageHeader
-          id="introduction"
-          className="scroll-m-20"
-          heading={frontmatter.name}
-        />
-
-        {content}
-
-        <hr className="my-4 md:my-6" />
-        <Pager slug={frontmatter.slug} hooks={hookList} />
-      </div>
-
-      <RightSidebar
-        toc={{
-          items: [
-            { title: 'Usage', url: '#usage' },
-            { title: 'API', url: '#api' },
-            { title: 'Hook', url: '#hook' },
-          ],
+    <>
+      <script
+        id="ld-json"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(ldJson),
         }}
       />
-    </main>
+
+      <main className="relative py-6 lg:gap-10 lg:py-10 xl:grid xl:grid-cols-[1fr_300px] xl:grid-rows-[auto_1fr_auto]">
+        <div className="mx-auto w-full min-w-0 grid">
+          <PageHeader
+            id="introduction"
+            className="scroll-m-20"
+            heading={frontmatter.name}
+          />
+
+          {content}
+
+          <hr className="my-4 md:my-6" />
+          <Pager slug={frontmatter.slug} hooks={hookList} />
+        </div>
+
+        <RightSidebar
+          toc={{
+            items: [
+              { title: 'Usage', url: '#usage' },
+              { title: 'API', url: '#api' },
+              { title: 'Hook', url: '#hook' },
+            ],
+          }}
+        />
+      </main>
+    </>
   )
 }
