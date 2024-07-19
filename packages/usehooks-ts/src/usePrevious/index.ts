53d31f9351f9cf12fb2b695070ccc6a6ae0e4f5a
Habib Ogunsola <ogunsolahabib@yahoo.com>
2024-07-19 22:15:20 +0000

    fix file names and import

--- packages/usehooks-ts/src/usePrevious/index.ts
+++ packages/usehooks-ts/src/usePrevious/index.ts
@@ -1,13 +1,1 @@
-import { useEffect, useRef } from 'react';
-
-export function usePrevious<T>(value: T): T | undefined {
-  const prevValueRef = useRef<T | undefined>();
-
-  useEffect(() => {
-
-    prevValueRef.current = value;
-
-  }, [value]);
-
-  return prevValueRef.current;
-}
+export * from './usePrevious'
