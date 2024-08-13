1a524113be467980cf1307d0eeb3f18ffada0cb4
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-08-13 14:20:33 +0900

    Feature: add useThrottle hook

---
+++ packages/usehooks-ts/src/useThrottle/useThrottle.md
@@ -0,0 +1,5 @@
+Use the throttle function with one simple function
+
+Related hooks:
+
+- [`useThrottle()`](/react-hook/use-throttle)
---
+++ packages/usehooks-ts/src/useThrottle/useThrottle.ts
@@ -0,0 +1,33 @@
+import { useRef } from 'react'
+
+/**
+ * Custom hook that provides a stottle function.
+ * @param {number} [defaultInterval] - The initial value for the throttle interval.
+ * @returns {(callback: () => void, interval?: number) => void} An executable throttle function
+ * a function to executed after a certain period of time based on the last function among the functions that are executed quickly.
+ * @public
+ * @see [Documentation](https://usehooks-ts.com/react-hook/use-throttle)
+ * @example
+ * ```tsx
+ * const throttle = useThrottle(2000); // Initial value is 2s
+ * // OR
+ * const throttle = useThrottle(); // Initial value is default value (1s)
+ * // Use throttle function in your component, Only one of the events that are processed redundantly can be processed.
+ * ```
+ */
+
+export function useThrottle(
+  defaultInterval = 1000,
+): (callback: () => void, interval?: number) => void {
+  const ref = useRef<NodeJS.Timeout>()
+
+  const throttle = (callback: () => void, interval?: number) => {
+    if (ref.current !== undefined) {
+      clearTimeout(ref.current)
+    }
+
+    ref.current = setTimeout(callback, interval ?? defaultInterval)
+  }
+
+  return throttle
+}
