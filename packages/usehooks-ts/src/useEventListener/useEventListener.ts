6f54d09937eb500f577c2f2423ff13ecb45644de
JimGitFE <rymsjfjrys@gmail.com>
2024-11-21 15:07:46 -0300

    typo

--- packages/usehooks-ts/src/useEventListener/useEventListener.ts
+++ packages/usehooks-ts/src/useEventListener/useEventListener.ts
@@ -98,8 +98,7 @@ function useEventListener<
     if (!(targetElement && targetElement.addEventListener)) return
 
     // Create event listener that calls handler function stored in ref
-    const listener: EventListener = event =>
-      savedHandler.current(event as Parameters<typeof handler>[0])
+    const listener: EventListener = event => savedHandler.current(event as Parameters<typeof handler>[0])
 
     targetElement.addEventListener(eventName, listener, config.options)
 
