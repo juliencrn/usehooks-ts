fb1d7033642c1cbe76ad958dedc08ed2d2088524
JimGitFE <rymsjfjrys@gmail.com>
2024-11-22 00:44:55 -0300

    doc: demo shorter demo

--- packages/usehooks-ts/src/useEventListener/useEventListener.demo.tsx
+++ packages/usehooks-ts/src/useEventListener/useEventListener.demo.tsx
@@ -5,9 +5,7 @@ import { useEventListener } from './useEventListener'
 export default function Component() {
   // Define button ref
   const buttonRef = useRef<HTMLButtonElement>(null)
-  const documentRef = useRef<Document>(document)
-  const speechSynthesis = new SpeechSynthesis()
-  const speechSynthesisRef = useRef(speechSynthesis)
+  const speechSynthesisRef = useRef(new SpeechSynthesis())
 
   const onScroll = (event: Event) => {
     console.log('window scrolled!', event)
@@ -28,16 +26,13 @@ export default function Component() {
   useEventListener('scroll', onScroll)
 
   // example with document based event
-  useEventListener('visibilitychange', onVisibilityChange, {element: documentRef })
+  useEventListener('visibilitychange', onVisibilityChange, {element: document }) // or useRef(document)
 
   // example with element based event
   useEventListener('click', onClick, { element: buttonRef })
 
   // example with EventTarget element based event
   useEventListener('voiceschanged', ev => console.log('speech!', ev), { element: speechSynthesisRef })
-
-  // Example without ref
-  useEventListener('voiceschanged', ev => console.log('speech!', ev), { element: speechSynthesis })
 
   return (
     <div style={{ minHeight: '200vh' }}>
